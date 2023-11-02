fed285a10b105b74ff9a229fbbfd75c9571f9314
teddy-vltn <123835861+teddy-vltn@users.noreply.github.com>
2023-11-02 01:28:23 +0100

    Feature: SpawnPoint

---
+++ entities/entities/bw_base_spawnpoint.lua
@@ -0,0 +1,62 @@
+DEFINE_BASECLASS("bw_base")
+
+AddCSLuaFile()
+
+ENT.PrintName       = "SpawnPoint"
+
+ENT.Model           = "models/props_trainstation/trainstation_clock001.mdl"
+ENT.Spawnable       = true
+ENT.AdminOnly       = true
+
+ENT.Modules = {
+    [0] = "SpawnPoint",
+    [1] = "Power"
+}
+
+local SpawnPointModule = nil
+local PowerModule = nil
+
+ENT.PowerUsage = 10
+ENT.PowerCapacity = 100
+
+function ENT:LoadConfig()
+    local spawnPointConfig = BaseWars.Config.Entities.SpawnPoints[self:GetClass()]
+
+    if not spawnPointConfig then
+        print("Error: SpawnPoint configuration not found for ", self:GetClass())
+        return
+    end
+
+    self.PowerUsage = spawnPointConfig.PowerUsage
+    self.PowerCapacity = spawnPointConfig.PowerCapacity
+end
+
+function ENT:Init()
+    if CLIENT then return end
+
+    self:LoadConfig()
+
+    SpawnPointModule = BaseWars.Entity.Modules:Get("SpawnPoint")
+    PowerModule = BaseWars.Entity.Modules:Get("Power")
+
+    self:SetPower(0)
+    self:SetPowerUsage(self.PowerUsage)
+    self:SetPowerCapacity(self.PowerCapacity)
+end
+
+function ENT:Think()
+    if CLIENT then return end
+
+    PowerModule:OnThink(self)
+
+end
+
+if SERVER then
+
+    function ENT:Use(ply)
+        local status, message = SpawnPointModule:UseFunc(self, ply)
+
+        BaseWars.Notify.Send(ply, "Activer le spawnpoint", message, status and Color(0, 255, 0) or Color(255, 0, 0))
+    end
+
+end
---
+++ gamemode/modules/entities/modules/sh_module_spawnpoint.lua
@@ -0,0 +1,52 @@
+local BW_SPAWNPOINT_MODULE = {}
+BW_SPAWNPOINT_MODULE.__index = BW_SPAWNPOINT_MODULE
+
+-- Constants
+local MODULE_NAME = "SpawnPoint"
+local MODULE_DESC = "Allows a player to set their spawn location."
+
+-- Initialization
+function BW_SPAWNPOINT_MODULE.New()
+    local self = setmetatable({}, BW_SPAWNPOINT_MODULE)
+    
+    self.Name = MODULE_NAME
+    self.Description = MODULE_DESC
+
+    return self
+end
+
+function BW_SPAWNPOINT_MODULE:Initialize(ent)
+    print("SpawnPoint module initialized for entity: ", ent)
+
+    if SERVER then
+        local ForceAngle = Angle(-90, 0, 0)
+        ent:SetAngles(ForceAngle)
+    end
+end
+
+function BW_SPAWNPOINT_MODULE:UseFunc(ent, activator, caller)
+
+    local ply = activator:IsPlayer() and activator or caller:IsPlayer() and caller or nil
+
+    if ply then
+        ent:EmitSound("buttons/blip1.wav")
+
+        ent.OwningPly = ply
+        ply.SpawnPoint = ent
+
+        return true, "Spawn point set!"
+    end
+
+    ent:EmitSound("buttons/button10.wav")
+
+    return false, "You must be a player to use this!"
+end
+
+function BW_SPAWNPOINT_MODULE:OnRemove(ent)
+    if IsValid(ent.OwningPly) then
+        ent.OwningPly.SpawnPoint = nil
+    end
+end
+
+local spawnPointModuleInstance = BW_SPAWNPOINT_MODULE.New()
+BaseWars.Entity.Modules:Add(spawnPointModuleInstance)
