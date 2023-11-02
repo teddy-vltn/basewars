9c81b55d5558a28acc92b0524321b49277b18918
teddy-vltn <123835861+teddy-vltn@users.noreply.github.com>
2023-11-02 01:56:58 +0100

    Feature : Auto Function Documentation

--- README.md
+++ README.md
@@ -5,3 +5,36 @@
 ## Installation
 
 ## Documentation
+function BaseWars.LoadEntityConfiguration()
+function BaseWars.NumberFormat(n)
+function BaseWars.Notify.Send(ply, title, message, color)
+function BaseWars.Notify.Broadcast(title, message, color)
+function BaseWars.SpawnMenu.GenerateUUID(item)
+function BaseWars.SpawnMenu.AddUUIDsToShop(categoryTable)
+function BaseWars.SpawnMenu.CollectItems(categoryTable, depth)
+function BaseWars.FlattenShop()
+function BaseWars.SpawnMenu.GetWeaponAutoBuy(ply)
+function BaseWars.SpawnMenu.BuyEntity(ply, uuid, pos, ang)
+function BaseWars.SpawnMenu.CalcPosAndAng(ply, ent)
+function BaseWars.SpawnMenu.DisableWeaponAutoBuy(ply)
+function BaseWars.SpawnMenu.ActivateWeaponAutoBuy(ply, uuid, state)
+function BaseWars.Faction.TryJoinFaction(name, password)
+function BaseWars.Faction.TryLeaveFaction(name)
+function BaseWars.Faction.TryCreateFaction(name, password, color, icon)
+function BaseWars.Faction.GetFactions()
+function BaseWars.Faction.GetFaction(name)
+function BaseWars.Faction.GetFactionByMember(ply)
+function BaseWars.Faction.HasFactionPassword(name)
+function BaseWars.Faction.Initialize() -- Initialisation
+function BaseWars.Faction.CreateFaction(name, leader, password, color, icon)
+function BaseWars.Faction.SyncFactions(ply)
+function BaseWars.Faction.SyncFaction(name, factionTable) -- Synchronisation
+function BaseWars.Faction.JoinFaction(ply, name, password)
+function BaseWars.Faction.LeaveFaction(ply)
+function BaseWars.Faction.DeleteFaction(name)
+function BaseWars.Faction.SetFaction(ply, name, leader)
+function BaseWars.Entity.Modules:Add(module)
+function BaseWars.Entity.Modules:Remove(name)
+function BaseWars.Entity.Modules:Get(name)
+
+## Contributing
---
+++ doc.py
@@ -0,0 +1,58 @@
+import os
+
+# Define the path to the directory containing the Lua files
+lua_files_directory = "gamemode/"  # Assuming the files are uploaded to the 'data' directory
+
+# Function to recursively find all Lua files
+def find_lua_files(directory):
+    lua_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.endswith('.lua'):
+                lua_files.append(os.path.join(root, file))
+    return lua_files
+
+# Function to extract function names
+def extract_function_names(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+    
+    # Pattern to look for lines that start with "function BaseWars."
+    pattern = "function BaseWars."
+    
+    # List to hold all function names
+    function_names = []
+    
+    for line in lines:
+        # Check if line starts with the pattern
+        if line.strip().startswith(pattern):
+            function_names.append(line.strip())
+    
+    return function_names
+
+# Find all Lua files
+lua_files = find_lua_files(lua_files_directory)
+
+# Extract function names from all Lua files
+all_function_names = []
+for lua_file in lua_files:
+    function_names = extract_function_names(lua_file)
+    all_function_names.extend(function_names)
+
+readme_file_path = "readme.md"
+with open(readme_file_path, 'r') as file:
+    lines = file.readlines()
+
+# Find the index of the Documentation section and Contributing section
+doc_start_index = next(i for i, line in enumerate(lines) if "## Documentation" in line) + 1
+contrib_index = next(i for i, line in enumerate(lines) if "## Contributing" in line)
+
+# Insert the function names into the lines list
+for name in reversed(all_function_names):  # Reverse to keep the order when inserting
+    lines.insert(doc_start_index, f"{name}\n")
+
+# Write the updated content back to the readme file
+with open(readme_file_path, 'w') as file:
+    file.writelines(lines)
+
+
