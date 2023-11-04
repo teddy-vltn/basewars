025899739fd8daf7784ee92d0888b46ddd92a2c8
teddy-vltn <123835861+teddy-vltn@users.noreply.github.com>
2023-11-04 01:53:52 +0100

    Fix: Removed recursive modules loading from shitred.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -19,104 +19,6 @@ function BaseWars.Log(...)
 	MsgN("")
 end
 
-function recursiveInclusion( scanDirectory, isGamemode )
-	-- Null-coalescing for optional argument
-	isGamemode = isGamemode or false
-	
-	local queue = { scanDirectory }
-	
-	-- Loop until queue is cleared
-	while #queue > 0 do
-		-- For each directory in the queue...
-		for _, directory in pairs( queue ) do
-			-- print( "Scanning directory: ", directory )
-			
-			local files, directories = file.Find( directory .. "/*", "LUA" )
-			
-			-- Include files within this directory
-			for _, fileName in pairs( files ) do
-				if fileName != "shared.lua" and fileName != "init.lua" and fileName != "cl_init.lua" then
-					-- print( "Found: ", fileName )
-					
-					-- Create a relative path for inclusion functions
-					-- Also handle pathing case for including gamemode folders
-					local relativePath = directory .. "/" .. fileName
-					if isGamemode then
-						relativePath = string.gsub( directory .. "/" .. fileName, GM.FolderName .. "/gamemode/", "" )
-					end
-					
-					-- Include server files
-					if string.match( fileName, "^sv" ) then
-						if SERVER then
-							include( relativePath )
-						end
-					end
-					
-					-- Include shared files
-					if string.match( fileName, "^sh" ) then
-						AddCSLuaFile( relativePath )
-						include( relativePath )
-					end
-					
-					-- Include client files
-					if string.match( fileName, "^cl" ) then
-						AddCSLuaFile( relativePath )
-						
-						if CLIENT then
-							include( relativePath )
-						end
-					end
-				end
-			end
-			
-			-- Append directories within this directory to the queue
-			for _, subdirectory in pairs( directories ) do
-				-- print( "Found directory: ", subdirectory )
-				table.insert( queue, directory .. "/" .. subdirectory )
-			end
-			
-			-- Remove this directory from the queue
-			table.RemoveByValue( queue, directory )
-		end
-	end
-end
-
-recursiveInclusion( GM.FolderName .. "/gamemode", true )
-
-/*
-			──────────────────██████────────────────
-			─────────────────████████─█─────────────
-			─────────────██████████████─────────────
-			─────────────█████████████──────────────
-			──────────────███████████───────────────
-			───────────────██████████───────────────
-			────────────────████████────────────────
-			────────────────▐██████─────────────────
-			────────────────▐██████─────────────────
-			──────────────── ▌─────▌────────────────
-			────────────────███─█████───────────────
-			────────────████████████████────────────
-			──────────████████████████████──────────
-			────────████████████─────███████────────
-			──────███████████─────────███████───────
-			─────████████████───██─███████████──────
-			────██████████████──────────████████────
-			───████████████████─────█───█████████───
-			──█████████████████████─██───█████████──
-			──█████████████████████──██──██████████─
-			─███████████████████████─██───██████████
-			████████████████████████──────██████████
-			███████████████████──────────███████████
-			─██████████████████───────██████████████
-			─███████████████████████──█████████████─
-			──█████████████████████████████████████─
-			───██████████████████████████████████───
-			───────██████████████████████████████───
-			───────██████████████████████████───────
-			─────────────███████████████────────────
-*/
-
-
 function BaseWars.Color(code)
 	return BaseWars.Config.Colors[code]
 end
@@ -183,6 +85,17 @@ function BaseWars.LoadEntityConfiguration()
 			end
         end
     end
+end
+
+include("sh_config.lua")
+
+include("modules/sh_modules.lua")
+
+if SERVER then
+	AddCSLuaFile("sh_config.lua")
+
+    AddCSLuaFile("modules/sh_modules.lua")
+
 end
 
 function GM:Initialize()
