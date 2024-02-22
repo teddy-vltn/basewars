2a03c4fc8038c017d1c31d8322d3160bc582da92
tvalenti <teddy.valentin@etudiant.univ-lr.fr>
2024-02-22 01:34:49 +0100

    Upgradeable props

--- gamemode/modules/props/sv_props.lua
+++ gamemode/modules/props/sv_props.lua
@@ -10,4 +10,45 @@ function BaseWars.Props.FindPlayerProps(ply)
     return props
 end
 
+-- On prop spawn define the health of the prop
+-- based on the gravity of the prop
+hook.Add("PlayerSpawnedProp", "BaseWars.Props.SetHealth", function(ply, model, ent)
+    local gravity = ent:GetPhysicsObject():GetMass() * 9.81
+    local health = gravity * 0.1
+
+    ent:SetMaxHealth(health)
+    ent:SetHealth(health)
+
+    -- Add the upgradeable module to the prop
+
+    -- A bit of a hacky way to add the upgradeable module to the prop
+    -- but it works
+    -- we normally would have to make it derive from the base entity bw_base for it to properly work
+    -- but we can't do that because it's a prop
+    -- so we have to add the module manually, gg no re
+    ent.Upgradeable = true
+    ent.UpgradeModule = BaseWars.Entity.Modules:Get("Upgradeable")
+
+    function ent:SetUpgradeLevel(level)
+        self:SetNWInt("UpgradeLevel", level)
+    end
+
+    function ent:GetUpgradeLevel()
+        return self:GetNWInt("UpgradeLevel")
+    end
+
+    function ent:Upgrade()
+        ent.UpgradeModule:Upgrade(ent)
+
+        local upgradeLevel = ent:GetUpgradeLevel()
+
+        ent:SetMaxHealth(health * upgradeLevel * 2)
+        ent:SetHealth(health * upgradeLevel * 2)
+    end
+
+    -- ent:Upgrade()
+end)
+
+
+
     
